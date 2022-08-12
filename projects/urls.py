20049aa63620755bc8e1704cfa7d923118931467
Ilfar <ishmatov_i@mail.ru>
2022-08-12 14:46:45 +0300

    created forms and added in views created,update

--- projects/urls.py
+++ projects/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.projects, name="projects"),
     path('project-object/<str:pk>/', views.project, name="project"),
+    path('create-project/', views.createProject, name="create-project"),
+    path('update-project/<str:pk>/', views.updateProject, name="update-project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Project
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Project, Tag
+from .forms import ProjectForm
 
 def projects(request):
     projects = Project.objects.all()
@@ -10,3 +11,27 @@ def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     tags = projectObj.tags.all()
     return render(request, 'projects/single-project.html', {'project': projectObj})
+
+def createProject(request):
+    form = ProjectForm()
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {'form': form}
+    return render(request, 'projects/project_forms.html', context)
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == "POST":
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {'form': form}
+    return render(request, 'projects/project_forms.html', context)
+
+
+
