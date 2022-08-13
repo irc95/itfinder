04ce65bf3bf01b21e0717d4bee6d19d111fd15ee
Ilfar <ishmatov_i@mail.ru>
2022-08-13 15:43:15 +0300

    added media files

--- projects/forms.py
+++ projects/forms.py
@@ -1,7 +1,25 @@
 from django.forms import ModelForm
 from .models import Project
+from django import forms
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'slug', 'tags', 'description', 'demo_link', 'source_link']
+        labels = {
+         'title': 'Название проекта',
+         'slug': 'Слаг',
+         'image':'Скриншот проекта',
+         'tags':'Теги',
+         'description':'Описание проекта',
+         'demo_link':'Демо-версия',
+         'source_link':'Исходный код на GitHub',
+         }
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+         }
+
+        def __init__(self, *args, **kwargs):
+            super(ProjectForm, self).__init__(*args, **kwargs)
+            for name, field in self.fields.items():
+                field.widget.attrs.update({'class': 'input'})
--- projects/views.py
+++ projects/views.py
@@ -7,15 +7,15 @@ def projects(request):
     context = {'projects': projects}
     return render(request, 'projects/projects.html', context)
 
-def project(request, pk):
-    projectObj = Project.objects.get(id=pk)
-    tags = projectObj.tags.all()
-    return render(request, 'projects/single-project.html', {'project': projectObj})
+def project(request, project_slug):
+    project = Project.objects.get(slug=project_slug)
+    tags = project.tags.all()
+    return render(request, 'projects/single-project.html', {'project': project})
 
 def createProject(request):
     form = ProjectForm()
     if request.method == "POST":
-        form = ProjectForm(request.POST)
+        form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('projects')
@@ -26,7 +26,7 @@ def updateProject(request, pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
     if request.method == "POST":
-        form = ProjectForm(request.POST, instance=project)
+        form = ProjectForm(request.POST, request.FILES, instance=project)
         if form.is_valid():
             form.save()
             return redirect('projects')
