b8f020d3bedf20119eecfc4df4c8abbec44499fc
Ilfar <ishmatov_i@mail.ru>
2022-08-14 15:39:23 +0300

    created login and logout

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import uuid
 from users.models import Profile
+from django.utils.text import slugify
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
@@ -11,6 +12,10 @@ class Tag(models.Model):
     def __str__(self):
         return self.name    
 
+    def save(self, *args, **kwargs):
+        value = self.name
+        self.slug = slugify(value, allow_unicode=True)
+        super().save(*args, **kwargs)
 
 class Project(models.Model):
     owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.CASCADE)
--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,12 @@ urlpatterns = [
     path('', views.profiles, name="profiles"),
     path('profile/<str:username>/', views.userProfile, name="user-profile"),
     path('skill/<slug:skill_slug>', views.profiles_by_skill, name="skill"),
+    path('login/', views.loginUser, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerUser, name="register"),  
+    path('account/', views.userAccount, name="account"),
+    path('edit-account/', views.editAccount, name="edit-account"),
+    path('create-skill/', views.createSkill, name="create-skill"),
+    path('update-skill/<slug:skill_slug>/', views.updateSkill, name="update-skill"),
+    path('delete-skill/<slug:skill_slug>/', views.deleteSkill, name="delete-skill"),
 ]
