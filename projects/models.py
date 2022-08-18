b5765dbc018e1d5f07d0da69baf4c1ef2ade84bd
Ilfar <ishmatov_i@mail.ru>
2022-08-18 12:12:43 +0300

    added message

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-import uuid
 from users.models import Profile
+import uuid
 from django.utils.text import slugify
 
 class Tag(models.Model):
@@ -9,16 +9,19 @@ class Tag(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
-    def __str__(self):
-        return self.name    
 
     def save(self, *args, **kwargs):
         value = self.name
         self.slug = slugify(value, allow_unicode=True)
         super().save(*args, **kwargs)
 
+    def __str__(self):
+        return self.name    
+
+
 class Project(models.Model):
-    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        Profile, null=True, blank=True, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     slug = models.SlugField()
     image = models.ImageField(null=True, blank=True, default="project_images/default.jpg", upload_to='project_images')
@@ -34,17 +37,41 @@ class Project(models.Model):
     def __str__(self):
         return self.title
 
+    @property
+    def reviewers(self):
+        queryset = self.review_set.all().values_list('owner__id', flat=True)
+        return queryset
+
+    @property
+    def getVoteCount(self):
+        reviews = self.review_set.all()
+        upVotes = reviews.filter(value='up').count()
+        totalVotes = reviews.count()
+
+        ratio = (upVotes / totalVotes) * 100
+        self.vote_total = totalVotes
+        self.vote_ratio = ratio
+
+        self.save()
+
+
 class Review(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
     VOTE_TYPE = (
-        ('up', 'Up Vote'),
-        ('down', 'Down Vote'),
-        )
-    review_text = models.TextField(null=True, blank=True)
+        ('up', 'Положительная оценка'),
+        ('down', 'Отрицательная оценка'),
+    )
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
     value = models.CharField(max_length=200, choices=VOTE_TYPE)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)    
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    class Meta:
+        unique_together = [['owner', 'project']]
 
     def __str__(self):
         return self.value
+
 
--- users/views.py
+++ users/views.py
@@ -5,23 +5,27 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.urls import conf
-from .models import Profile, Skill
-from .forms import CustomUserCreationForm, ProfileForm, SkillForm
+from .models import Profile, Skill, Message
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm, MessageForm
+from .utils import paginateProfiles, searchProfiles
+
 
 
 def loginUser(request):
     page = 'login'
+
     if request.user.is_authenticated:
         return redirect('profiles')
+
     if request.method == 'POST':
-        username = request.POST('username')
-        password = request.POST('password')
+        username = request.POST['username']
+        password = request.POST['password']
 
         try:
             user = User.objects.get(username=username)
         except:
             messages.error(request, 'Такого пользователя нет в системе')
-        
+
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
@@ -63,11 +67,11 @@ def registerUser(request):
     context = {'page': page, 'form': form}
     return render(request, 'users/login_register.html', context)
 
-
-
 def profiles(request):
-    profiles = Profile.objects.all()
-    context = {'profiles': profiles}
+    profiles, search_query = searchProfiles(request)
+    custom_range, profiles = paginateProfiles(request, profiles, 6)
+    context = {'profiles': profiles, 'search_query': search_query,
+               'custom_range': custom_range}
     return render(request, 'users/profiles.html', context)
 
 
@@ -90,26 +94,29 @@ def profiles_by_skill(request, skill_slug):
 
     return render(request, "users/profiles.html", context)
 
-
 @login_required(login_url='login')
 def userAccount(request):
     profile = request.user.profile
+
     skills = profile.skills.all()
-    projects = profile.prject_set.all()
+    projects = profile.project_set.all()
+
     context = {'profile': profile, 'skills': skills, 'projects': projects}
     return render(request, 'users/account.html', context)
-
 
 
 @login_required(login_url='login')
 def editAccount(request):
     profile = request.user.profile
     form = ProfileForm(instance=profile)
-    if request.method == "POST":
+
+    if request.method == 'POST':
         form = ProfileForm(request.POST, request.FILES, instance=profile)
         if form.is_valid():
             form.save()
+
             return redirect('account')
+
     context = {'form': form}
     return render(request, 'users/profile_form.html', context)
 
@@ -118,7 +125,7 @@ def editAccount(request):
 def createSkill(request):
     profile = request.user.profile
     form = SkillForm()
- 
+
     if request.method == 'POST':
         form = SkillForm(request.POST)
         if form.is_valid():
@@ -128,7 +135,7 @@ def createSkill(request):
             profile.skills.get_or_create(name=skill, slug=skill_slug, description=skill_description)
             messages.success(request, 'Навык добавлен')
             return redirect('account')
- 
+
     context = {'form': form}
     return render(request, 'users/skill_form.html', context)
 
@@ -138,18 +145,18 @@ def updateSkill(request, skill_slug):
     profile = request.user.profile
     skill = profile.skills.get(slug=skill_slug)
     form = SkillForm(instance=skill)
- 
+
     if request.method == 'POST':
         form = SkillForm(request.POST, instance=skill)
         if form.is_valid():
             form.save()
             messages.success(request, 'Навык успешно обновлен')
             return redirect('account')
- 
+
     context = {'form': form}
     return render(request, 'users/skill_form.html', context)
- 
- 
+
+
 @login_required(login_url='login')
 def deleteSkill(request, skill_slug):
     profile = request.user.profile
@@ -158,11 +165,55 @@ def deleteSkill(request, skill_slug):
         skill.delete()
         messages.success(request, 'Навык успешно удален')
         return redirect('account')
- 
+
     context = {'object': skill}
     return render(request, 'delete_template.html', context)
 
 
+@login_required(login_url='login')
+def inbox(request):
+    profile = request.user.profile
+    messageRequests = profile.messages.all()
+    unreadCount = messageRequests.filter(is_read=False).count()
+    context = {'messageRequests': messageRequests, 'unreadCount': unreadCount}
+    return render(request, 'users/inbox.html', context)
+
+
+@login_required(login_url='login')
+def viewMessage(request, pk):
+    profile = request.user.profile
+    message = profile.messages.get(id=pk)
+    if message.is_read == False:
+        message.is_read = True
+        message.save()
+    context = {'message': message}
+    return render(request, 'users/message.html', context)
 
 
 
+def createMessage(request, username):
+    recipient = Profile.objects.get(username=username)
+    form = MessageForm()
+
+    try:
+        sender = request.user.profile
+    except:
+        sender = None
+
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.sender = sender
+            message.recipient = recipient
+
+            if sender:
+                message.name = sender.name
+                message.email = sender.email
+            message.save()
+
+            messages.success(request, 'Your message was successfully sent!')
+            return redirect('user-profile', username=recipient.username)
+
+    context = {'recipient': recipient, 'form': form}
+    return render(request, 'users/message_form.html', context)    
